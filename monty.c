f4deee83c08d0931a3051dc3d1c08e67aaa4b4cc
Codesmth <reyclamar06@gmail.com>
2023-12-17 11:19:40 +0000

    pshar pstr

--- monty.c
+++ monty.c
@@ -56,37 +56,45 @@ int main(int argc, char *argv[])
 		{
 			pint(&stack, line_number);
 		}
-		else if (strcmp(opcode, "pop") == 0)
+		else if (opcode && strcmp(opcode, "pop") == 0)
 		{
 			pop(&stack, line_number);
 		}
-		else if (strcmp(opcode, "swap") == 0)
+		else if (opcode && strcmp(opcode, "swap") == 0)
 		{
 			swap(&stack, line_number);
 		}
-		else if (strcmp(opcode, "add") == 0)
+		else if (opcode && strcmp(opcode, "add") == 0)
 		{
 			add(&stack, line_number);
 		}
-		else if (strcmp(opcode, "nop") == 0)
+		else if (opcode && strcmp(opcode, "nop") == 0)
 		{
 			nop(&stack, line_number);
 		}
-		else if (strcmp(opcode, "sub") == 0)
+		else if (opcode && strcmp(opcode, "sub") == 0)
 		{
 			sub(&stack, line_number);
 		}
-		else if (strcmp(opcode, "div") == 0)
+		else if (opcode && strcmp(opcode, "div") == 0)
 		{
 			div_op(&stack, line_number);
 		}
-		else if (strcmp(opcode, "mul") == 0)
+		else if (opcode && strcmp(opcode, "mul") == 0)
 		{
 			mul(&stack, line_number);
 		}
-		else if (strcmp(opcode, "mod") == 0)
+		else if (opcode && strcmp(opcode, "mod") == 0)
 		{
 			mod(&stack, line_number);
+		}
+		else if (opcode && strcmp(opcode, "pchar") == 0)
+		{
+			pchar(&stack, line_number);
+		}
+		else if (opcode && strcmp(opcode, "pstr") == 0)
+		{
+			pstr(&stack, line_number);
 		}
 
 		free(line);
---
+++ pchar.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * pchar - Prints the char at the top of the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number in the Monty byte code file.
+ */
+
+void pchar(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    int ascii_value = (*stack)->n;
+
+    if (ascii_value < 0 || ascii_value > 127)
+    {
+        fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    putchar(ascii_value);
+    putchar('\n');
+
+    pop(stack, line_number);
+}
